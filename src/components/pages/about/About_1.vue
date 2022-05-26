f84aeae85d256cd7292af718504e4366bdcb6f19
Surajudeen <aderojus@gmail.com>
2022-05-26 20:41:15 +0100

    workpage updated

--- src/components/pages/about/About_1.vue
+++ src/components/pages/about/About_1.vue
@@ -31,6 +31,141 @@
                         </b-col>
                 </b-row>
         <!-- aboutpage section1 ends here -->
+
+        <!-- aboutpage section2 start here -->
+        <div class="aboutpage__section_2 p-5">
+                <b-row class="col-12  mx-auto  project">
+                    <b-col cols="6" class="px-5">
+                        <div class="text-justify d-flex align-items-end pl-2">
+                           <p class="pl-5 pr-5 customer_acquisition"> 
+                               Using Marshal our customers have been able to avoid the common potential 
+                               lawsuits from ADA and other web accessibilities laws
+                            
+                           </p>
+                        </div>
+                        
+                         
+                        </b-col>
+                        <b-col cols="6" class="">
+                           
+                        <div class="mb-5 pt-4 text-left">
+                            <router-link  to="/" class="ml-5 project_btn ">
+                                <img  class=""  src="@/assets/images/left-button.png" height="90px">
+                            </router-link>
+                        </div>
+                           
+                        </b-col>
+                </b-row>
+                <div class="tronweb-container p-5 mx-auto d-inline-flex">
+                  <div class="col-12 tronweb_id_card_letter_headed  pl-5">
+                        <div class="mb-5 left_image">
+                           
+                            <img class=" pt-5 position-absolute id_card" src="@/assets/images/ID CARD A 1.png">
+                             <img class="  pl-5 letter_headed" src="@/assets/images/Letterhead 1.png">
+                             
+                        </div>
+                        
+                        <div class="tronweb_logo p-5 text-center">
+                            <img class="p-3 j tronweb_logo_image" src="@/assets/images/Tronweb Logo A.png">
+                        </div>
+                      
+                  </div>
+ 
+                    
+                </div>
+
+                <b-row class="col-12 pt-5 mb-5 mx-auto  project">
+                    <b-col cols="6" class="px-5">
+                        <div class="text-justify d-flex align-items-end pl-2 pt-5">
+                           <p class="pl-5 pr-5 customer_acquisition pt-5"> 
+                              See all the great things you can do for your business with 
+                              Tronweb Suite of Business Tech Solutions to keep your business ahead – all round!
+                           </p>
+                        </div>
+                        
+                         
+                        </b-col>
+                        <b-col cols="6" class="pt-5">
+                           
+                        <div class="mb-5 pt-5 text-left">
+                            <router-link  to="/" class="ml-5 project_btn ">
+                                <img  class=""  src="@/assets/images/left-button.png" height="90px">
+                            </router-link>
+                        </div>
+                           
+                        </b-col>
+                </b-row>
+ 
+                
+        
+        </div>
+        <!-- aboutpage section2 start here -->
+
+        <!--aboutpage section3 start here -->
+        <div class="aboutpage__section_3 p-5">
+            <div class="d-inline-flex">
+                <div class="col-12 mb-5 left_image">
+                
+                    <img class="  pl-5 position-absolute referal_image" src="@/assets/images/referal_image2.png" width="40%"> 
+                    <img class="pl-5 referal_image_1 w-100" src="@/assets/images/referal_image1.png">
+                </div>
+                
+                <div class="col-4 pl-5  p-5 vira_referral">
+                    <img class="mb-5" src="@/assets/images/aboutpage_logo.png">
+                   <p class="pl-5 ">
+                    The Viral Referral System that is based on Rewards
+                    to customers for referrals. This word-of-mouth technique will help you generate traffic,
+                   </p>
+                </div>
+    
+            </div>    
+            
+            
+        </div>
+
+        <!-- aboutpage section3 end here -->
+        <!-- aboutpage section4 start here -->
+        <div class="aboutpage__section_4 p-5">
+            <div class="tronweb-container p-5 mx-auto d-inline-flex">
+                <div class="col-12  pl-5">
+                    <div class="mb-5 left_image">
+                        <img class="pt-5 women_holding_phone_image" width="55%" src="@/assets/images/woman_holding_phone.png">
+                            <img class="software_images" width="73%" src="@/assets/images/software_image.png">
+                            <img class="image_bottom" width="55%" src="@/assets/images/login_form_buttom.png">  
+                    </div>
+                </div>  
+            </div>
+            <b-row class="col-12 pt-3 mb-5 mx-auto  project">
+                    <b-col cols="6" class="px-5">
+                        <div class="text-justify d-flex align-items-end pl-2">
+                           <p class="pl-5 pr-5 customer_acquisition "> 
+                               This Brand New Software Turns You Into A Content 
+                               Master By Helping You To Generate Cash Sucking Content
+                            </p>
+                        </div>
+                        
+                         
+                        </b-col>
+                        <b-col cols="6" class="">
+                           
+                        <div class="mb-5  text-center">
+                            <router-link  to="/" class="ml-5 project_btn ">
+                                <img  class=""  src="@/assets/images/view_project_button.png" height="90px">
+                            </router-link>
+                        </div>
+                           
+                     </b-col>
+                </b-row>
+ 
+
+        </div>
+
+        <!-- aboutpage section4 end here -->
+        <!-- abooutpage section5 start here -->
+        <div class="aboutpage__section_5 p-5">
+            nnnn
+        </div>
+        <!-- aboutpage  section5 end here -->
      </div>
 </template>
 
@@ -61,4 +196,83 @@ export default {
     line-height:50px;
 }
 
+.aboutpage__section_2{
+    background-color: #11271F;
+    position: relative;
+    color: #fff;
+}
+.tronweb_id_card_letter_headed{
+     background-image: url('../../../assets/images/tronweb_bg.svg');
+      background-repeat: no-repeat;
+    
+      margin-top:100px;
+      
+      .letter_headed{
+       margin-left:270px;
+       margin-top:-100px
+           
+      }
+      .id_card{
+        margin-top:170px;
+        margin-left:-114px;   
+      }
+      .tronweb_logo_image{
+          margin-left:300px;
+          margin-top:70px;
+          
+      }
+}
+
+.aboutpage__section_3{
+    background-color: #DCDCDC;
+    position: relative;
+    color: #000000
+}
+
+.referal_image_1{
+       
+       margin-top:-100px;
+       margin-left:-114px;   
+           
+      }
+      .vira_referral{
+          margin-top:300px;
+          margin-left:600px;
+          position:absolute;
+          font-size:18px;
+          
+      }
+.aboutpage__section_4{
+    background-color: #11271F14;
+    position: relative;
+    color: #000000
+}
+    
+      .software_images{
+       
+         position: absolute;
+        margin-left:-390px;
+        margin-top:-25px
+
+           
+      }
+      .women_holding_phone_image{
+          margin-left:0px;
+          margin-top:90px
+      
+      }
+      .image_bottom{
+          margin-top:-130px;
+      }
+
+    .curve_curve_image{
+        margin-left:0px; 
+    
+    }
+    .aboutpage__section_5{
+    background-color: #DCDCDC;
+    position: relative;
+    color: #000000
+}
+      
 </style>
