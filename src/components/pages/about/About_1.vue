74b6b172ae6ffbe3510145b2bf629bca2b3a8710
Surajudeen <aderojus@gmail.com>
2022-05-27 23:22:07 +0100

    aboutpage updated

--- src/components/pages/about/About_1.vue
+++ src/components/pages/about/About_1.vue
@@ -12,7 +12,7 @@
         <b-row class="col-12  mx-auto  project">
                     <b-col cols="6" class="px-5">
                         <div class="text-justify d-flex align-items-end pl-2">
-                           <p class="pl-5 pr-5 customer_acquisition"> Clientforce is customer acquisition and sales 
+                           <p class="pl-5 pr-5 customer_acquisition1"> Clientforce is customer acquisition and sales 
                             automation platform that helps you find your 
                             ideal customers with a few clicks.
                            </p>
@@ -29,82 +29,113 @@
                         </div>
                            
                         </b-col>
+               
                 </b-row>
+
+            <div class="m-5 p-5">
+                <img src="@/assets/images/curve_buttom_image.png">
+           </div>
         <!-- aboutpage section1 ends here -->
 
+        <!-- section1overlap image -->
+        <b-row class="d-inline-flex overlap_image">
+            
+            <b-col col-sm="12" cols="8">
+                <img src="@/assets/images/tablet.png" width="100%">
+                
+            </b-col>
+           <b-col class="mobile_tablet_bottom_logo  position-absolute">
+                    <img src="@/assets/images/m_logo.png" width="10%" class="m_logo">
+            </b-col>
+             <b-col c0l-sm="12" cols="1" class="mobile">
+                 <img src="@/assets/images/mobile.png">
+            </b-col>
+            
+        </b-row>
+
+        <!-- section2_overlap_section between two & three -->
+        
         <!-- aboutpage section2 start here -->
-        <div class="aboutpage__section_2 p-5">
-                <b-row class="col-12  mx-auto  project">
-                    <b-col cols="6" class="px-5">
-                        <div class="text-justify d-flex align-items-end pl-2">
-                           <p class="pl-5 pr-5 customer_acquisition"> 
-                               Using Marshal our customers have been able to avoid the common potential 
-                               lawsuits from ADA and other web accessibilities laws
+    <div class="aboutpage__section_2 p-5">
+            <b-row class="col-12  mx-auto  project m-5 p-5">
+                <b-col cols="6" class="px-5">
+                    <div class="text-justify d-flex align-items-end pl-2">
+                        <p class="pl-5 pr-5 customer_acquisition "> 
+                            Using Marshal our customers have been able to avoid the common potential 
+                            lawsuits from ADA and other web accessibilities laws
+                        
+                        </p>
+                    </div>
+                    
+                        
+                    </b-col>
+                    <b-col cols="6" class="">
+                        
+                    <div class="mb-5 pt-4 text-left">
+                        <router-link  to="/" class="pl-5 project_btn ">
+                            <img  class=""  src="@/assets/images/left-button.png" height="90px">
+                        </router-link>
+                    </div>
+                        
+                    </b-col>
+            </b-row>
+            <div class="tronweb-container p-5 mx-auto d-inline-flex">
+                <div class="col-12 tronweb_id_card_letter_headed  pl-5">
+                    <div class="mb-5 left_image">
+                        
+                        <img class=" pt-5 position-absolute id_card" src="@/assets/images/ID CARD A 1.png">
+                            <img class="  pl-5 letter_headed" src="@/assets/images/Letterhead 1.png">
                             
-                           </p>
-                        </div>
-                        
-                         
-                        </b-col>
-                        <b-col cols="6" class="">
-                           
-                        <div class="mb-5 pt-4 text-left">
-                            <router-link  to="/" class="ml-5 project_btn ">
-                                <img  class=""  src="@/assets/images/left-button.png" height="90px">
-                            </router-link>
-                        </div>
-                           
-                        </b-col>
-                </b-row>
-                <div class="tronweb-container p-5 mx-auto d-inline-flex">
-                  <div class="col-12 tronweb_id_card_letter_headed  pl-5">
-                        <div class="mb-5 left_image">
-                           
-                            <img class=" pt-5 position-absolute id_card" src="@/assets/images/ID CARD A 1.png">
-                             <img class="  pl-5 letter_headed" src="@/assets/images/Letterhead 1.png">
-                             
-                        </div>
-                        
-                        <div class="tronweb_logo p-5 text-center">
-                            <img class="p-3 j tronweb_logo_image" src="@/assets/images/Tronweb Logo A.png">
-                        </div>
-                      
-                  </div>
- 
+                    </div>
+                    
+                    <div class="tronweb_logo p-5 text-center">
+                        <img class="p-3 j tronweb_logo_image" src="@/assets/images/Tronweb Logo A.png">
+                    </div>
                     
                 </div>
 
-                <b-row class="col-12 pt-5 mb-5 mx-auto  project">
-                    <b-col cols="6" class="px-5">
-                        <div class="text-justify d-flex align-items-end pl-2 pt-5">
-                           <p class="pl-5 pr-5 customer_acquisition pt-5"> 
-                              See all the great things you can do for your business with 
-                              Tronweb Suite of Business Tech Solutions to keep your business ahead – all round!
-                           </p>
-                        </div>
-                        
-                         
-                        </b-col>
-                        <b-col cols="6" class="pt-5">
-                           
-                        <div class="mb-5 pt-5 text-left">
-                            <router-link  to="/" class="ml-5 project_btn ">
-                                <img  class=""  src="@/assets/images/left-button.png" height="90px">
-                            </router-link>
-                        </div>
-                           
-                        </b-col>
-                </b-row>
- 
                 
-        
-        </div>
-        <!-- aboutpage section2 start here -->
+            </div>
 
+            <b-row class="col-12 pt-5 mb-5 mx-auto  project">
+                <b-col cols="6" class="px-5">
+                    <div class="text-justify d-flex align-items-end pl-2 pt-5">
+                        <p class="pl-5 pr-5 great_thing pt-5 pb-5"> 
+                            See all the great things you can do for your business with 
+                            Tronweb Suite of Business Tech Solutions to keep your business ahead – all round!
+                        </p>
+                    </div>
+                    
+                        
+                    </b-col>
+                    <b-col cols="6" class="pt-5">
+                        
+                    <div class="mb-5 pt-5 ">
+                        <router-link  to="/" class="ml-5 project_btn ">
+                            <img  class=""  src="@/assets/images/left-button.png" height="90px">
+                        </router-link>
+                    </div>
+                        
+                    </b-col>
+            </b-row>
+
+            
+    
+    </div>
+    <!-- aboutpage section2 start here -->
+    
+        
         <!--aboutpage section3 start here -->
+        
+        
+        <!-- overlap between section2 and section3 ends here -->
+
+
+
+
         <div class="aboutpage__section_3 p-5">
-            <div class="d-inline-flex">
-                <div class="col-12 mb-5 left_image">
+            <div class="d-inline-flex section_3_4_overlap_image mb-5 ">
+                <div class="col-12 mb-5">
                 
                     <img class="  pl-5 position-absolute referal_image" src="@/assets/images/referal_image2.png" width="40%"> 
                     <img class="pl-5 referal_image_1 w-100" src="@/assets/images/referal_image1.png">
@@ -119,11 +150,36 @@
                 </div>
     
             </div>    
-            
+           
+              <div class="tronweb-container_2 p-5 mx-auto d-inline-flex">
+                <b-row class="p-5 smiley_group">
+                    <b-col cols-sm="12" cols="7" class="pt-5 ">
+                        <img class="mb-5" src="@/assets/images/smiley_man.png" height="600px">
+                        <img class="smiley_man_overlap mb-5" height="400px" src="@/assets/images/smiley_man_overlap.png">
+                    </b-col>
+                    <b-col cols-sm="12" cols="4" class="pt-5 this_brand_new_software">
+                        <div class="pt-5">
+                            <p class="pt-5">
+                            This Brand New Software Turns You Into A Content 
+                            Master By Helping You To Generate Cash Sucking Content
+                            </p>
+                        </div>
+                        <div class="mb-5 ">
+                        <router-link  to="/" class="ml-5 project_btn ">
+                            <img  class=""  src="@/assets/images/view_project_button.png" height="70px">
+                        </router-link>
+                    </div>
+                    </b-col>
+                    
+                </b-row>  
+            </div>
+           
             
         </div>
-
-        <!-- aboutpage section3 end here -->
+         <!-- aboutpage section3 end here -->
+         
+        
+       
         <!-- aboutpage section4 start here -->
         <div class="aboutpage__section_4 p-5">
             <div class="tronweb-container p-5 mx-auto d-inline-flex">
@@ -135,10 +191,10 @@
                     </div>
                 </div>  
             </div>
-            <b-row class="col-12 pt-3 mb-5 mx-auto  project">
+            <b-row class="col-12 pt-3 mb-5 mx-auto ">
                     <b-col cols="6" class="px-5">
                         <div class="text-justify d-flex align-items-end pl-2">
-                           <p class="pl-5 pr-5 customer_acquisition "> 
+                           <p class="pl-5 pr-5 brand_new_software "> 
                                This Brand New Software Turns You Into A Content 
                                Master By Helping You To Generate Cash Sucking Content
                             </p>
@@ -163,10 +219,57 @@
         <!-- aboutpage section4 end here -->
         <!-- abooutpage section5 start here -->
         <div class="aboutpage__section_5 p-5">
-            nnnn
+            <div class="testimonail__section">
+                 <div class="" >
+                    <b-row cols="2" class="my-5 col-9 mx-auto testimonial__section">
+                        <b-col class="testimonial__section-left p-4" cols="6">
+                            <P class="text-justify">
+                                “I’d highly recommend TheBrander as a web design agency, having worked with them on the design and development of a new website over the past 3-4 months.
+                                From initial brand development and refinement, to the design and implementation of statements that it’s can’t be done! ”
+                            </P>
+                            <img  class="float-right mt-5 " src="@/assets/images/group_img.png" style="height:80px">
+                        </b-col>
+                        <b-col class="testimonial__section-right p-4 d-flex flex-column justify-content-between">
+                            <h4 class="m-0 p-0">Testimonial</h4>
+                            <p class="">
+                                Williams Shakesspared <br>
+                                ~ Founder of Being A Company
+                            </p>
+                        </b-col>
+                    </b-row>
+                </div>
+                
+                <div class="col-12  mx-auto my-3 project">
+                        
+                        <div class="text-justify d-flex align-items-end">
+                            <img style="" src="@/assets/images/zigzag_image.png" height="250px"> 
+                            <h1 class="pl-5 project_title quando">Have a project in mind? Let’s get to work.</h1>
+
+                           
+                        </div>
+                        <div class="mb-5 pt-4">
+                            <router-link  to="/" class="ml-5 project_btn">
+                                Start A Project
+                            </router-link>
+                        </div>
+                        
+                        
+                       
+                </div>
+            </div>
+            <!-- debrander agency game ends here -->
+        
+        </div>
+        
+        <!-- workpage section4 end here -->
+
+        <!-- bottom addon -->
+        <img src="@/assets/images/big_wave.png" alt="" class="big_wave">
+
+            
         </div>
         <!-- aboutpage  section5 end here -->
-     </div>
+    
 </template>
 
 <script>
@@ -194,6 +297,21 @@ export default {
 .customer_acquisition{
     font-size:22px;
     line-height:50px;
+    margin-top:250px;
+}
+.text-left {
+    margin-top:250px;
+}
+.mobile{
+    margin-left:-100px;
+}
+.mobile_tablet_bottom_logo{
+     background-image: url('../../../assets/images/tablet_mobile_logo_bg.png');
+      background-repeat: no-repeat;
+      background-size:150px;
+      margin-left:700px;
+      margin-top:400px
+    
 }
 
 .aboutpage__section_2{
@@ -223,6 +341,11 @@ export default {
       }
 }
 
+.section_3_4_overlap_image{
+    position:absolute;
+    top:-10px;
+
+}
 .aboutpage__section_3{
     background-color: #DCDCDC;
     position: relative;
@@ -242,6 +365,26 @@ export default {
           font-size:18px;
           
       }
+      .smiley_group{
+          margin-top:600px;
+          .smiley_man{
+              
+          }
+          .smiley_man_overlap{
+              position:absolute;
+              bottom:-160px;
+              left:250px;
+          }
+
+          
+      }
+      .this_brand_new_software{
+          margin-top:50px;
+          margin-left:-100px
+      }
+.left_image_1{
+    margin-top:400px;
+}
 .aboutpage__section_4{
     background-color: #11271F14;
     position: relative;
@@ -265,14 +408,51 @@ export default {
           margin-top:-130px;
       }
 
-    .curve_curve_image{
-        margin-left:0px; 
-    
-    }
+   
     .aboutpage__section_5{
     background-color: #DCDCDC;
     position: relative;
     color: #000000
 }
-      
+      .testimonail__section {
+        .project {
+            &_title {
+                font-size: 60px;
+                font-weight: 400;
+            }
+            &_btn {
+                background: #FEAC5E;
+                justify-content: center;
+                color: #000;
+                border-radius: 2px;
+                padding: 10px 20px;
+                font-size:16px;
+                &:hover {
+                    text-decoration: none;
+                    background: #faae68;
+                }
+            }
+        }
+        .testimonial__section {
+            font-size:20px;
+            
+            border-bottom: 1px solid  #000000;
+            &-right {
+                border-left :1px solid  #000000;
+            }
+        }
+    }
+    .big_wave {
+    position: absolute;
+    bottom: -120px;
+    right: 0;
+    z-index: 1;
+}
+.overlap_image{
+    position: absolute;
+    top: 1400px;
+    right: 0;
+    z-index: 1
+
+}
 </style>
