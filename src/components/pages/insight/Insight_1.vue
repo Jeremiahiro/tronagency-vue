3531ad783f0aa0de57f315fd70a498252042254c
Surajudeen <aderojus@gmail.com>
2022-06-02 08:11:53 +0100

    work and about page complete

--- src/components/pages/insight/Insight_1.vue
+++ src/components/pages/insight/Insight_1.vue
@@ -33,6 +33,7 @@
       </b-row>
       <!-- insight section_1 end here -->
       <img src="@/assets/images/ring_group.svg" height="150px" class="float-right ring_group">
+      <!-- insight section_1 atart here -->
       <div class="insight_section_2 p-5">
           <b-row class="">
               <b-col cols-sm="12" cols="6">
@@ -47,18 +48,141 @@
                   <img src="@/assets/images/frame_image.png"  height="300px" class="w-100  position-absolute frame_image ">
                   
               </b-col>
-          </b-row>
+            
+            </b-row>
+            <b-row class="m-0 p-0">
+                <b-col cols-sm="12" cols="12" class="">
+                    <img src="@/assets/images/layer_1.png" width="" class="mx-5 w-100">
+                </b-col>
+            </b-row>
 
-          
+            <b-row class="">
+              <b-col cols-sm="12" cols="6">
+                  <img src="@/assets/images/white_lady.png" class="mx-5">
+                  <b-row class="d-inline-flex two_image_beside position-absolute">
+                      <b-col cols="6">
+                          <img class="mt-5  left_image_pic" src="@/assets/images/overlap_right_image.svg">                           
+                      </b-col>
+                      <b-col cols="6">
+                          <img class="ml-5 mx-5 mt-5 position-absolute right-image-pic" src="@/assets/images/overlap_left_image.svg">
+                      </b-col>
+                  </b-row>
+ 
+              </b-col>
+              <b-col cols-sm="12" cols="6" class="p-5 pt-5 mt-5">
+                  <h1 class="quando pl-5 pt-5">Brand identities that last.</h1>
+                  <p class="mx-5 my-5 offer">
+                      Via educated strategies and standout visual identity design, we can expand your 
+                      digital reach and help you create lasting bonds with your customers.
+                  </p>
+              </b-col>
+            
+            </b-row>
+            <b-row class="m-0 p-0">
+                <b-col cols-sm="12" cols="12" class="">
+                    <img src="@/assets/images/layer_1.png" width="" class="mx-5 w-100">
+                </b-col>
+            </b-row>
+            <b-row class="">
+                <b-col cols="6">
+                    <h1 class="quando mx-5">Systemic design for everyday products.</h1>
+                    <p class="mx-5 offer">We can build new digital products from the ground up or upgrade existing ones to 
+                        cope with new demands and a growing user base. Using extensive research, data, and testing, 
+                        we design and deploy human-centric experiences that are intuitive and easy to use.
+                    </p>
+                </b-col>
+                 <b-col cols="6" class="pt-5 mt-5">
+                    <img src="@/assets/images/Dashboard.png" class="dashboard">
+                </b-col>
+
+            </b-row>
+            <b-row class="">
+                <b-col cols="3" class="pt-5 mt-5">
+                    <img src="@/assets/images/beads.png" width="130px" class="mx-5">
+                </b-col>
+                <b-col cols="9">
+                    <img src="@/assets/images/black__white_men.png" height="400px" class="w-100">
+                </b-col>
+            </b-row>
       </div>
+      <!-- insight section_2 end here -->
+
+      <!-- insight section_3 start here -->
+        <div class="insight__section__3 p-5">
+          <!--div class="">
+              <h1>Our Field Of Expertise </h1>
+              <h1>& capabilities
+                  <span class=""><p>50+ — projects we’ve helped launch through the years. Take a look at some of our selected case studies.</p></span>
+              </h1>
+              
+          </div-->
+
+          <b-row class="mx-5 service_offer">
+              <b-col cols="6">
+                <h1>Digital experiences</h1>
+                <ul class="">
+                    <li>Marketing website</li>
+                    <li>Digital products</li>
+                    <li>Mobile apps</li>
+                    <li>Design systems</li>
+                    <li>interactions and animations</li>
+                    <li>Illustrations and iconography</li>
+                </ul>
+              </b-col>
+               <b-col cols="6">
+                <h1>Branding</h1>
+                <ul>
+                    <li>Finds and opportunities</li>
+                    <li>Brand strategy</li>
+                    <li>Identity development</li>
+                    <li>Digital and print collateral</li>
+                    <li>Brand guidelines</li>
+                </ul>
+              </b-col>
+               <b-col cols="5" class="mx-5 pt-5">
+                <h1>Engineering</h1>
+                <ul>
+                    <li>Front-end development</li>
+                    <li>Website development</li>
+                    <li>Product development</li>
+                    <li>CMS implementation</li>
+                    <li>Quality assurance</li>
+                </ul>
+              </b-col>
+               <b-col cols="6" class="pt-5">
+                <h1>Discovery</h1>
+                <ul>
+                    <li>Competitive benchmarking</li>
+                     <li>Customer research and data analysis</li>
+                     <li>Stakeholder and user interviews</li>
+                     <li>User flows and customer journey</li>
+                     <li>Information Architecture</li>
+                </ul>
+              </b-col>
+           </b-row>
+           <div class="p-5 m-3">
+              <Insight2 /> 
+           </div> 
+        </div>
+        <div class="insight_section_5_bg">
+            <b-row class="result__board">
+                <b-col cols="6" class="pl-5 ">
+                    <h4 class="result px-5">The results we deliver.</h4>
+                </b-col>
+                <b-col cols="6" class="my-5">
+                    <img src="@/assets/images/percentage_board.png" width="60%" class="mx-5 ml-5 percentage_board position-relative">
+                </b-col>
+            </b-row>
+        </div>
 
      </div>
 </div>
     
 </template>
 <script>
+import Insight2 from './Insight_2.vue'
 export default {
-    
+     components: { Insight2 },
 }
 </script>
 <style lang="scss" scoped>
@@ -84,7 +208,7 @@ export default {
     
     .quando{
         font-family: 'Quando', serif;
-        font-size: 60px;
+        font-size: 50px;
         
 
         
@@ -92,7 +216,7 @@ export default {
     .insight_section_2{
         background-color: #FEAC5E80;
         .offer{
-            font-size:28px
+            font-size:23px
         }
         .new_person_frame_image{
             margin-top:-50px;
@@ -102,11 +226,48 @@ export default {
             }
 
         }
+        .two_image_beside{
+            bottom:-130px;
+            left:185px;
+            .right-image-pic{
+                left:165px
+            }
+        }
+        .dashboard{
+           // 
+        }
         
     }
+    .insight__section__3{
+        background: #DCDCDC;
+        .service_offer{
+            // background-image: url("../../../assets/images/long.png");
+        }
+        ul{
+            line-height: 70px;
+        }
+        
 
+    }
+    .insight_section_5_bg{
+        background-image: url('../../../assets/images/mom__baby.svg');
+        background-repeat: no-repeat;
+        &_result__board{
+            
+        }
+        .result{
+            color:#fff;
+            font-family: 'quando' serif;
+            font-size:38px
+        }
+        .percentage_board{
+            padding-top:500px
+        }
+    }
     .ring_group{
         margin-top:-60px
     }
+
+    
 }
 </style>
---
+++ src/components/pages/insight/Insight_2.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="testimonail__section">
+        <div class="" >
+            <b-row cols="2" class="my-5 col-9 mx-auto testimonial__section">
+                <b-col class="testimonial__section-left p-4" cols="6">
+                    <P class="text-justify">
+                        “TheBrander Agency was a game changer for Teachable — we used them to design our homepage (and have used that for many years) and they were the first people we turned to when we needed to build a marketplace. 
+                        I highly recommend them and our team loves working with them.”                      </P>
+                    <img  class="float-right mt-5 " src="@/assets/images/debrander-game-image.png" style="height:80px">
+                </b-col>
+                <b-col class="testimonial__section-right p-4 d-flex flex-column justify-content-between">
+                    <h4 class="m-0 p-0">100k+ — creators using <br>
+                    Teachable every day.
+                    </h4>
+                    <p class="">
+                        Anthony Raden<br>
+                        ~ Founder of Being A Company
+                    </p>
+                </b-col>
+            </b-row>
+        </div>
+</div>
+            
+</template><script>
+export default {
+    
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+.testimonail__section {
+        .project {
+            &_title {
+                font-size: 60px;
+                font-weight: 400;
+            }
+            &_btn {
+                background: #FEAC5E;
+                justify-content: center;
+                color: #000;
+                border-radius: 2px;
+                padding: 10px 20px;
+                font-size:16px;
+                &:hover {
+                    text-decoration: none;
+                    background: #faae68;
+                }
+            }
+        }
+        .testimonial__section {
+            font-size:20px;
+            
+            border-bottom: 1px solid  #000000;
+            &-right {
+                border-left :1px solid  #000000;
+            }
+        }
+    }
+
+</style>
